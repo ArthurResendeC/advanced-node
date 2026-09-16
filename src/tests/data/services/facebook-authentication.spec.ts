0859e4fd5b4347caa04edfd39f5c05d52d611d51
Arthur Resende <arthur.resendecoura@hotmail.com>
2026-09-16 15:38:14 -0300

    ensure service calls  once

--- src/tests/data/services/facebook-authentication.spec.ts
+++ src/tests/data/services/facebook-authentication.spec.ts
@@ -5,11 +5,13 @@ import { describe, it, expect } from 'bun:test';
 
 class LoadFacebookUserApiSpy implements LoadFacebookUserApi {
   token?: string;
+  callsCount = 0;
   result = undefined;
   async loadUser(
     params: LoadFacebookUserApi.Params,
   ): Promise<LoadFacebookUserApi.Result> {
     this.token = params.token;
+    this.callsCount++;
     return this.result;
   }
 }
@@ -23,6 +25,7 @@ describe('FacebookAuthenticationService', () => {
     await sut.perform({ token: 'any_token' });
 
     expect(loadFacebookUserApi.token).toBe('any_token');
+    expect(loadFacebookUserApi.callsCount).toBe(1);
   });
 
   it('should return AuthenticationError when LoadFacebookUserApi returns undefined', async () => {
